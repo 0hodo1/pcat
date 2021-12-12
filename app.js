7f93c2cd896e8457f58286cea3ce76c1f620f831
Ömer Hodo <omerhodo@gmail.com>
2021-12-12 13:12:22 +0300

    db connection success

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const path = require('path');
 
+const Photo = require('./models/Photo');
+
 const app = express();
+
+// connect db
+mongoose.connect('mongodb://localhost:27017/pcat-test-db');
 
 // template engine
 app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -18,6 +27,11 @@ app.get('/about', (req, res) => {
 });
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
